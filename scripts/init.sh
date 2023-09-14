659bd100c49aabb85fe08654fc0e110d1ad2a402
mathias <mathiasgheno@gmail.com>
2023-09-13 23:23:03 -0300

    feat: block execution to not root

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]; then
+  echo "Please, run it as root";
+  exit 1
+fi
+
 ls ../ | grep router.json
 if [ $? -ne 0 ]; then
   echo "You need to provide an router.json file"
   exit 1
 fi
 
-jq . router.json
+jq . ../router.json
 if [ $? -ne 0 ]; then
   echo "File router.json has sintax error"
   exit 1
@@ -15,7 +20,7 @@ fi
 echo "Making all scripts executable"
 
 find ./ -type f -name "*.sh" -exec chmod +x {} \;
-if [ $? -ne 0]; then
+if [ $? -ne 0 ]; then
   echo "Error giving permissions to files"
   exit 1
 fi
